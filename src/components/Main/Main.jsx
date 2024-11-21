15c21e79307d1a464a5b5cd440c9dd80b80794a9
BSenichak <bsenk77@gmail.com>
2024-11-21 12:27:00 +0200

    start

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import style from './Main.module.scss'
+
+function Main(props) {
+  return (
+    <main className={style.wrapper}>Main</main>
+  )
+}
+
+Main.propTypes = {}
+
+export default Main
