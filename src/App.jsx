38c63f8b4efac31bae8efb163bb9b9dc82ae1c92
BSenichak <bsenk77@gmail.com>
2024-11-22 14:50:48 +0200

    18 lesson done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import Footer from "./components/Footer/Footer"
-import Header from "./components/Header/Header"
-import Main from "./components/Main/Main"
-import "./styles/generalStyle.scss"
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import "./styles/generalStyle.scss";
 
 function App() {
-  return (
-    <>
-      <Header/>
-      <Main />
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import style from './Main.module.scss'
+import React from "react";
+import PropTypes from "prop-types";
+import style from "./Main.module.scss";
+import AddForm from "../AddForm/AddForm";
+import { FaRegCalendarPlus } from "react-icons/fa";
 
 function Main(props) {
-  return (
-    <main className={style.wrapper}>Main</main>
-  )
+    const [modalOpen, setModalOpen] = React.useState(false);
+    return (
+        <main className={style.wrapper}>
+{modalOpen && <AddForm open={setModalOpen} />}
+<button
+    className={style.addButton}
+    onClick={() => setModalOpen(true)}
+>
+    <FaRegCalendarPlus />
+</button>
+        </main>
+    );
 }
 
-Main.propTypes = {}
+Main.propTypes = {};
 
-export default Main
+export default Main;
