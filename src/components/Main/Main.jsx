8c7e706ebc36c88d148d6491ef3f6226d57855c6
BSenichak <bsenk77@gmail.com>
2024-12-03 13:12:38 +0200

    add login and registration page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,6 +8,8 @@ import { Routes, Route } from "react-router";
 import WCalendar from "../WCalendar/WCalendar";
 import DCalendar from "../DCalendar/DCalendar";
 import Modal from "../Modal/Modal";
+import LoginPage from "../Auth/LoginPage";
+import RegisterPage from "../Auth/RegisterPage";
 
 function Main(props) {
     const [modalOpen, setModalOpen] = React.useState(false);
@@ -25,6 +27,8 @@ function Main(props) {
                 <Route path="/month" element={<MCalendar />} />
                 <Route path="/week" element={<WCalendar />} />
                 <Route path="/day" element={<DCalendar />} />
+                <Route path="/login" element={<LoginPage/>} />
+                <Route path="/register" element={<RegisterPage/>} />
             </Routes>
         </main>
     );
