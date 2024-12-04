13f3fef6b7e0640ccc7c268d3ac2b752566775f2
BSenichak <bsenk77@gmail.com>
2024-12-04 17:56:09 +0200

    add store

--- src/components/DCalendar/DCalendar.jsx
+++ src/components/DCalendar/DCalendar.jsx
@@ -2,10 +2,8 @@ import React, { useContext, useState } from "react";
 import PropTypes from "prop-types";
 import style from "./DCalendar.module.scss";
 import { ContextStore } from "../../store/ContextStore";
-import { useRequireAuth } from "../../hooks/useRequireAuth";
 
 function DCalendar(props) {
-    useRequireAuth();
     let { events } = useContext(ContextStore);
     const [currentDate, setCurrentDate] = useState(new Date());
 
--- src/components/WCalendar/WCalendar.jsx
+++ src/components/WCalendar/WCalendar.jsx
@@ -2,10 +2,8 @@ import React, { useContext, useState } from "react";
 import PropTypes from "prop-types";
 import style from "./WCalendar.module.scss";
 import { ContextStore } from "../../store/ContextStore";
-import { useRequireAuth } from "../../hooks/useRequireAuth";
 
 function WCalendar(props) {
-    useRequireAuth();
     let { events } = useContext(ContextStore);
     const [currentDate, setCurrentDate] = useState(new Date());
 
