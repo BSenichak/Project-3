b47d452907afdb36378c616e5986914b64320514
BSenichak <bsenk77@gmail.com>
2024-12-05 20:15:26 +0200

    one more update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
 dotenv.config();
 
