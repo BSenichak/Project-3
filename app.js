1fb7e72e2401f5e868dfab54181bb9193d65d207
BSenichak <bsenk77@gmail.com>
2024-12-05 19:47:32 +0200

    update

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.delete("/remove/:id", authenticateToken, async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
